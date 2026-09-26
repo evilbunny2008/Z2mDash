066cddcdae6ce9a850b3e690af46f40518e9eea3
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-09-26 23:25:49 +1000

    flush

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.odiousapps.z2mdash"
         minSdk = 26
         targetSdk = 37
-        versionCode = 46
-        versionName = "0.0.46"
+        versionCode = 47
+        versionName = "0.0.47"
     }
 
     buildTypes {
